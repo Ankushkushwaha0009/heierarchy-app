e617bfd4b79d186658d0fd529c97e29603a4e722
Ankush kushwaha <ankushkushwaha2828@gmail.com>
2024-01-24 12:18:28 +0530

    some file has changed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import "./App.css";
 
 function App() {
+
+  const [selectedNodeType, setSelectedNodeType] = useState("singleNode");
+  const [data , newdata]  = useState("") ; 
+
+  const handleNodeTypeChange = (event) => {
+    setSelectedNodeType(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>{data}</h1>
+      <label htmlFor="nodetype"> Node Type </label>
+      <select name="nodetype" id="nodetype" onChange={handleNodeTypeChange} value={selectedNodeType}>
+        <option value="singleNode">SingleNode</option>
+        <option value="ParentNode">ParentNode</option>
+        <option value="ChildNode">ChildNode</option>
+      </select>
+
+      <label> Selected Node </label>
+      <input type="text" placeholder="Node Name" onChange={e => newdata(e.target.value)} value={data}/>
+      <button> Update </button>
+      <button> Delete </button>
+
+      <h2> Node value</h2>
+      <input type="text" placeholder="write node value here " />
+      <button> Add Node </button>
     </div>
   );
 }
